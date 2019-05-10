03a13734b8369b82803a89a27878b6e9cea428bb
toxtli <ctoxtli@gmail.com>
2019-05-10 06:40:32 -0400

    Join files

---
+++ data/build.sh
@@ -0,0 +1,9 @@
+cat Face_Visual.pkl.* > Face_Visual.pkl
+rm Face_Visual.pkl.*
+cat Face_Visual_Test.pkl.* > Face_Visual_Test.pkl
+rm Face_Visual_Test.pkl.*
+cat Body_Visual.pkl.* > Body_Visual.pkl
+rm Body_Visual.pkl.*
+cat Body_Visual_Test.pkl.* > Body_Visual_Test.pkl
+rm Body_Visual_Test.pkl.*
+echo Done
